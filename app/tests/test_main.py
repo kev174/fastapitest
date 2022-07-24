f24b29527a52724e879cc38fc8baab7b5cb8c3ab
kev174 <kev17404@yahoo.co.uk>
2022-07-24 14:58:22 +0100

    updated with tests and aws keys yml

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,6 +3,6 @@ from main import app
 
 client = TestClient(app)
 
-#def test_main_resource():
-    #response_auth = client.get(f"/")
-    #assert response_auth.status_code == 200
+def test_main_resource():
+    response_auth = client.get(f"/inventory/foo")
+    assert response_auth.status_code == 200
